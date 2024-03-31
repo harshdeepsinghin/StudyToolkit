70ca4689d62a727061080c77583b37a58e77bff9
Harshdeep Singh <94488766+harshdeepsinghin@users.noreply.github.com>
2024-03-31 16:59:04 +0530

    Dumping and Loading data done with styling

--- study.py
+++ study.py
@@ -4,6 +4,7 @@ import time
 import os
 import platform
 import click
+from colorama import Fore, Style
 from dotenv import load_dotenv
 
 # Environments loading
@@ -12,6 +13,7 @@ GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 api_hash = os.getenv('API_HASH')
 
 # Variables Declaration
+
 TOPICS = set()
 TERM_DEFINITION = dict()
 TOPIC_DESCRIPTION = dict()
@@ -72,15 +74,51 @@ def note(TYPE):
         ANSWER = input("Answer: ")
         QUESTION_ANSWER.update({QUESTION:ANSWER})
     elif ( TYPE == 4 ):  # 2.4 - Miscellaneous Notes
-        if ( PLATFORM in ('Darwin','Linux')):
-            print("Unix/Linux")
-            click.edit(filename='misc_notes.md')
+        if ( PLATFORM in ('Darwin','Linux','Unix')):
+            print("Unix/Linux/Darwin")
+            click.edit(filename='misc_notes.txt')
         elif ( PLATFORM == 'Windows' ):
-            print("Windows")
-        NOTE = input("Note: ")
-        MISC_NOTES.append(NOTE)
+            print("To be implemented.") #TODO
 
+    save_data()
     return 0
+
+
+## View Formatting Function
+def print_with_color_and_format(text, color, bold=False, underline=False):
+    style = ''
+    if bold:
+        style += '\033[1m'
+    if underline:
+        style += '\033[4m'
+    style += color
+    print(style + text + Style.RESET_ALL)
+
+## Study Notes function
+def study_notes(TYPE):
+    if TYPE == 1:    # 2.1 - Term-Definition
+        print_with_color_and_format("Term Definitions:", Fore.YELLOW, bold=True)
+        for term, definition in TERM_DEFINITION.items():
+            print_with_color_and_format(f"\t{term}", Fore.YELLOW, bold=True)
+            print_with_color_and_format(f"\t►  {definition}\n", Fore.YELLOW)
+    elif TYPE == 2:  # 2.2 - Topic-Description
+        print_with_color_and_format("Topic Descriptions:", Fore.GREEN, bold=True)
+        for topic, description in TOPIC_DESCRIPTION.items():
+            print_with_color_and_format(f"\t{topic}", Fore.GREEN, bold=True)
+            print_with_color_and_format(f"\t►  {description}\n", Fore.GREEN)
+    elif TYPE == 3:  # 2.3 - Question-Answer
+        print_with_color_and_format("Question-Answers:", Fore.CYAN, bold=True)
+        for question, answer in QUESTION_ANSWER.items():
+            print_with_color_and_format(f"\t{question}", Fore.CYAN, bold=True)
+            print_with_color_and_format(f"\t►  {answer}\n", Fore.CYAN)
+    elif TYPE == 4:  # 2.4 - Miscellaneous Notes
+        print_with_color_and_format("Miscellaneous Notes:", Fore.MAGENTA, bold=True)
+        with open("misc_notes.txt", 'r') as MN:
+            for line in MN:
+                print_with_color_and_format(f"\t• {line.strip()}", Fore.MAGENTA)
+    return 0
+
+
 
 def search_term():
     while True:
@@ -159,19 +197,43 @@ def askai():
 
         return 0
 
+## Load Data Function
+def load_data():
+    global TERM_DEFINITION
+    global TOPIC_DESCRIPTION
+    global QUESTION_ANSWER
+    try:
+        with open("term_definition.json", 'r') as TED:
+            TERM_DEFINITION = json.load(TED)
+    except FileNotFoundError:
+        pass
+
+    try:
+        with open("topic_description.json", 'r') as TOD:
+            TOPIC_DESCRIPTION = json.load(TOD)
+    except FileNotFoundError:
+        pass
+
+    try:
+        with open("question_answer.json", 'r') as QA:
+            QUESTION_ANSWER = json.load(QA)
+    except FileNotFoundError:
+        pass
+    return 0
+
+
 
 ## Save Data Function
 def save_data():
-
-    with open("Question_Answer.md", 'a') as QA:
-        for Q,A in QUESTION_ANSWER.items():
-            QA.write(f"Question: {Q}\n")
-            QA.write(f"Answer: {A}\n\n")
-
+    with open("term_definition.json", 'w') as TED, open("topic_description.json", 'w') as TOD, open("question_answer.json", 'w') as QA:
+        json.dump(TERM_DEFINITION, TED, indent=4)
+        json.dump(TOPIC_DESCRIPTION, TOD, indent=4)
+        json.dump(QUESTION_ANSWER, QA, indent=4)
     return 0
 
 ## Main function
 def main():
+    load_data()
     menu(0)
     while True:
 
@@ -194,23 +256,26 @@ def main():
                     Q=int(input("\nSelect an option: "))
                     if ( Q == 1 ):    # 1.2.1 - Term-Definition
                         Q = -1
-                        print(TERM_DEFINITION)
+                        study_notes(1)
 
                     elif ( Q == 2 ):  # 1.2.2 - Topic-Description
                         Q = -1
-                        print(TOPIC_DESCRIPTION)
+                        study_notes(2)
 
                     elif ( Q == 3 ):  # 1.2.3 - Question-Answer
                         Q = -1
-                        print(QUESTION_ANSWER)
+                        study_notes(3)
 
                     elif ( Q == 4 ):  # 1.2.4 - Miscellaneous Notes
                         Q = -1
-                        print(MISC_NOTES)
+                        study_notes(4)
 
                     elif ( Q == 5 ):  # 1.2.5 - All Notes
                         Q = -1
-                        print(TERM_DEFINITION, TOPIC_DESCRIPTION, QUESTION_ANSWER, MISC_NOTES)
+                        study_notes(1)
+                        study_notes(2)
+                        study_notes(3)
+                        study_notes(4)
 
             elif ( Q == 2 ):    # 2 - Note
                 Q = -1
